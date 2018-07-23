dcc9492fe4f791daf6f48e6b5d80aff5a6d1441d
3ddesign <3ddesign@bk.ru>
2018-07-23 21:35:59 +0300

    test

--- p2/src/app/app.component.ts
+++ p2/src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  public myModel = ''
-  public mask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]
-
+export class AppComponent implements OnInit{
+  public arr = ['active', 'active', 'panding', 'panding']
   constructor() {}
+
+  ngOnInit() {
+    this.arr.forEach(function(item, i) {
+      console.log(`${item} index: ${++i}`);
+    });
+  }
 
 }
