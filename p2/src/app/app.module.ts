e5c98bf9f0c7a8e03c1a1c2a8690f6720fc033a3
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-02-19 07:19:36 +0200

    test ngx-translations for lazy load modules

--- p2/src/app/app.module.ts
+++ p2/src/app/app.module.ts
@@ -6,13 +6,30 @@ import { AppComponent } from './app.component';
 
 export const APP_ID = 'my-app';
 
+// import ngx-translate and the http loader
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+
 @NgModule({
   imports: [
     BrowserModule.withServerTransition({ appId: APP_ID }),
     AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+  })
   ],
   exports: [ AppRoutingModule ],
   providers: [],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
