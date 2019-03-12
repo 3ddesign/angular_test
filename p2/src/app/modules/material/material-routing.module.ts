b261f468a567f84476f028185f53657f468e11c0
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-03-12 07:40:41 +0200

    test grapecity

--- p2/src/app/modules/material/material-routing.module.ts
+++ p2/src/app/modules/material/material-routing.module.ts
@@ -1,15 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { MaterialComponent } from './material.component';
+import { MaterialComponent } from "./material.component";
 
-const routes: Routes = [
-  { path: '', component: MaterialComponent },
-];
+const routes: Routes = [{ path: "", component: MaterialComponent }];
 
 @NgModule({
-  declarations: [ ],
-  imports: [ RouterModule.forChild(routes) ],
-  exports: [ RouterModule ],
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
-export class MaterialRoutingModule { }
+export class MaterialRoutingModule {}
