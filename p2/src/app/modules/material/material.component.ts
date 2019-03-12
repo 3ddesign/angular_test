0937ac4506a4bcd09671fb48f6d6a500dc8412e6
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-03-12 07:40:56 +0200

    test grapecity

--- p2/src/app/modules/material/material.component.ts
+++ p2/src/app/modules/material/material.component.ts
@@ -1,12 +1,12 @@
-import { Component } from "@angular/core";
-import { FormControl } from "@angular/forms";
+import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
-  selector: "app-material",
-  templateUrl: "./material.component.html",
-  styleUrls: ["./material.component.css"]
+  selector: 'app-material',
+  templateUrl: './material.component.html',
+  styleUrls: ['./material.component.css'],
 })
 export class MaterialComponent {
   myControl = new FormControl();
-  options: string[] = ["One", "Two", "Three"];
+  options: string[] = ['One', 'Two', 'Three'];
 }
