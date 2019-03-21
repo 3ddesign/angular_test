229a3bfc9bad10d9b9b2c244a04b00da5906f0e8
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-03-21 07:06:43 +0200

    test autocomlete

--- p2/src/app/modules/material/material-routing.module.ts
+++ p2/src/app/modules/material/material-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { MaterialComponent } from './material.component';
 
 const routes: Routes = [
-  { path: '', component: MaterialComponent },
+  { path: '**', component: MaterialComponent },
 ];
 
 @NgModule({
