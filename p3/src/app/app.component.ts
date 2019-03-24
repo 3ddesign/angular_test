6a2daea6d0b5cfa8e714fcfc6679d165d6af5223
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-03-24 14:14:49 +0200

    set grapecity

--- p3/src/app/app.component.ts
+++ p3/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'p3';
-}
+  spreadBackColor = 'aliceblue';
+  sheetName = 'Goods List';
+
+  hostStyle = {
+  width: '800px',
+  height: '600px'
+  };
+
+  data = [
+  {Name: 'Apple', Category: 'Fruit', Price: 1, 'Shopping Place': 'Wal-Mart'},
+  {Name: 'Potato', Category: 'Fruit', Price: 2.01, 'Shopping Place': 'Other'},
+  {Name: 'Tomato', Category: 'Vegetable', Price: 3.21, 'Shopping Place': 'Other'},
+  {Name: 'Sandwich', Category: 'Food', Price: 2, 'Shopping Place': 'Wal-Mart'},
+  {Name: 'Hamburger', Category: 'Food', Price: 2, 'Shopping Place': 'Wal-Mart'},
+  {Name: 'Grape', Category: 'Fruit', Price: 4, 'Shopping Place': 'Sun Store'}
+  ];
+  columnWidth = 100;
+
+  }
--- p3/src/app/app.module.ts
+++ p3/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SpreadSheetsModule } from '../assets/lib/spreadjs/gc.spread.sheets.angular.12.0.9';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    SpreadSheetsModule,
     BrowserModule
   ],
   providers: [],
