63a6d83f46d59ca3541c49280000c9429036d419
Dmitry <3ddesign@bk.ru>
2020-04-22 08:23:23 +0300

    add timer without rxjs

--- mathform/src/app/equation/equation.component.ts
+++ mathform/src/app/equation/equation.component.ts
@@ -9,6 +9,8 @@ import { MathValidators } from '../math-validators';
   styleUrls: ['./equation.component.scss']
 })
 export class EquationComponent implements OnInit {
+  secondsPerSolution = 0;
+
   mathForm = new FormGroup(
     {
       a: new FormControl(this.randomNumber()),
@@ -29,7 +31,17 @@ export class EquationComponent implements OnInit {
   }
 
   ngOnInit() {
+    const startTime = new Date();
+    let numberSolved = 0;
+
+
+
     this.mathForm.statusChanges.pipe(filter(value => value === 'VALID'), delay(100)).subscribe(() => {
+      numberSolved++;
+      this.secondsPerSolution = (
+        new Date().getTime() - startTime.getTime()
+      ) / numberSolved / 1000;
+
        this.mathForm.setValue({
         a: this.randomNumber(),
         b: this.randomNumber(),
