11407e5e632abe2a86488facb6a2c7cf9fe4378e
Dmitry <3ddesign@bk.ru>
2020-04-23 07:50:49 +0300

    change countdown with scan operator

--- mathform/src/app/equation/equation.component.ts
+++ mathform/src/app/equation/equation.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import { delay, filter } from 'rxjs/operators';
+import { delay, filter, scan } from 'rxjs/operators';
 import { MathValidators } from '../math-validators';
 
 @Component({
@@ -31,13 +31,14 @@ export class EquationComponent implements OnInit {
   }
 
   ngOnInit() {
-    const startTime = new Date();
-    let numberSolved = 0;
-
-
-
-    this.mathForm.statusChanges.pipe(filter(value => value === 'VALID'), delay(100)).subscribe(() => {
-      numberSolved++;
+    this.mathForm.statusChanges.pipe(filter(value => value === 'VALID'),
+     delay(100),
+     scan((acc) => {
+        return {
+          numberSolved: acc.numberSolved + 1,
+          startTime: acc.startTime
+        }
+     }, { numberSolved: 0, startTime: new Date() })).subscribe(( { numberSolved, startTime }) => {
       this.secondsPerSolution = (
         new Date().getTime() - startTime.getTime()
       ) / numberSolved / 1000;
