6861a0bcb569a62673d2c423a4e815ae21e7f212
Dmitry <3ddesign@bk.ru>
2020-04-26 00:51:29 +0300

    add calc of right result

--- mathform/src/app/answer-highlight.directive.ts
+++ mathform/src/app/answer-highlight.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, ElementRef, OnInit } from '@angular/core';
 import { NgControl } from '@angular/forms';
+import { map, filter } from 'rxjs/operators';
 
 @Directive({
   selector: '[appAnswerHighlight]'
@@ -9,7 +10,10 @@ export class AnswerHighlightDirective implements OnInit {
   constructor(private el: ElementRef, private controlName: NgControl) { }
 
   ngOnInit() {
-    console.log(this.controlName.control.parent);
+     this.controlName.control.parent.valueChanges.pipe(map(({ a, b, answer }) => Math.abs((a + b - answer) / (a +b))))
+     .subscribe(value => {
+      console.log(value);
+     })
   }
 
 }
