8a1d8a9d0c96b6d6a0580a5b185dc658fc6c60c3
Dmitry <3ddesign@bk.ru>
2020-04-29 07:51:31 +0300

    generate modules and components

---
+++ emailclient/src/app/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+
+
+@NgModule({
+  declarations: [SigninComponent, SignupComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
