c8e75e8db376efa9fd805a3191f0170ff8efe40e
Dmitry <3ddesign@bk.ru>
2020-04-30 07:56:32 +0300

    add init routes

--- emailclient/src/app/auth/auth-routing.module.ts
+++ emailclient/src/app/auth/auth-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'signup', component: SignupComponent },
+  { path: '', component: SigninComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
