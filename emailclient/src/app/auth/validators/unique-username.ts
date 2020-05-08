3bb8eb82e8af928d9cb672e30d86af0f0649af69
Dmitry <3ddesign@bk.ru>
2020-05-08 07:53:36 +0300

    add catchError

--- emailclient/src/app/auth/validators/unique-username.ts
+++ emailclient/src/app/auth/validators/unique-username.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AsyncValidator, FormControl } from '@angular/forms';
-
+import { map, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class UniqueUsername implements AsyncValidator {
@@ -12,6 +13,19 @@ export class UniqueUsername implements AsyncValidator {
 
     return this.httpClient.post<any>('https://api.angular-email.com/auth/username', {
         username: value
-    });
+    }).pipe(
+      map(value => {
+          if (value.available) {
+            return null;
+          }
+        }),
+        catchError(error => {
+          if (error.message.username) {
+            return of({ nonUniqueUsername: true });
+          } else {
+            return of({ noConnection: true });
+          }
+        })
+      );
   }
 }
