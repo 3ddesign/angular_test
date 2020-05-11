8ba1966c66722f23c4eed9e9bbaec2f42c5176d2
Dmitry <3ddesign@bk.ru>
2020-05-11 09:13:37 +0300

    add robust error handling

--- emailclient/src/app/shared/input/input.component.ts
+++ emailclient/src/app/shared/input/input.component.ts
@@ -15,4 +15,9 @@ export class InputComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showErros() {
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
+  }
+
 }
