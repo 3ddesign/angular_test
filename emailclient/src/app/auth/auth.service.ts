a3a84ef18d9edf273ff83cc2c7d86a2ec2ae1f82
Dmitry <3ddesign@bk.ru>
2020-05-15 08:04:16 +0300

    clean authService

--- emailclient/src/app/auth/auth.service.ts
+++ emailclient/src/app/auth/auth.service.ts
@@ -5,20 +5,31 @@ interface UserNaveAvailableResponse {
   available: boolean
 }
 
+interface SignupCredentials {
+  username: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+interface SignupResponse {
+  username: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  rootUrl = 'https://api.angular-email.com'
 
   constructor(private httpClient: HttpClient) { }
 
   usernameAvaliable(username: string) {
-    return this.httpClient.post<UserNaveAvailableResponse>('https://api.angular-email.com/auth/username', {
+    return this.httpClient.post<UserNaveAvailableResponse>(`${this.rootUrl}/auth/username`, {
         username
     })
   }
 
-  signup(credentials: any) {
-    return this.httpClient.post<any>('https://api.angular-email.com/auth/signup', credentials);
+  signup(credentials: SignupCredentials) {
+    return this.httpClient.post<SignupResponse>(`${this.rootUrl}/auth/signup`, credentials);
   }
 }
