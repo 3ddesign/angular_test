c6d30f972f29b2fbca598978cc9cd598716b7a27
Dmitry <3ddesign@bk.ru>
2020-05-23 08:58:47 +0300

    add checkAuth

--- emailclient/src/app/app.component.ts
+++ emailclient/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
 import { BehaviorSubject } from 'rxjs';
 
@@ -7,11 +7,14 @@ import { BehaviorSubject } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   signedin$: BehaviorSubject<boolean>
 
   constructor(private authService: AuthService) {
     this.signedin$ = this.authService.signedin$;
   }
 
+  ngOnInit() {
+    this.authService.checkAuth().subscribe(() => {});
+  }
 }
