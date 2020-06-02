6c075c5866f22ca1ff466277a56cea70efaeada5
Dmitry <3ddesign@bk.ru>
2020-06-02 08:32:58 +0300

    add inbox module

--- emailclient/src/app/app-routing.module.ts
+++ emailclient/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'inbox',
+  loadChildren: () => import('./inbox/inbox.module').then(module => module.InboxModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
