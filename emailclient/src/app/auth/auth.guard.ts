517983189f69f4026be87b331bb46a90f0660c1e
Dmitry <3ddesign@bk.ru>
2020-06-05 08:07:10 +0300

    set guard

--- emailclient/src/app/auth/auth.guard.ts
+++ emailclient/src/app/auth/auth.guard.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanLoad, Route, UrlSegment, Router
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { skipWhile, take, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+export class AuthGuard implements CanLoad {
+  constructor(private authService: AuthService, private router: Router) {}
+canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.signedin$.pipe(
+      skipWhile(value => value === null),
+      take(1),
+      tap((authenticated) => { if (!authenticated) this.router.navigateByUrl('/') })
+    );
   }
-  
 }
