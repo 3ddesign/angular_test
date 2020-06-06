c56989925a78b5d89f4e9d1eab8fa91787b64f6f
Dmitry <3ddesign@bk.ru>
2020-06-06 07:09:47 +0300

    add inbox components

---
+++ emailclient/src/app/inbox/email-index/email-index.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-email-index',
+  templateUrl: './email-index.component.html',
+  styleUrls: ['./email-index.component.scss']
+})
+export class EmailIndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
