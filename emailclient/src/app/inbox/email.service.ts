226094eb4af82cbc673d98e200b7cb93e456423c
Dmitry <3ddesign@bk.ru>
2020-07-04 07:07:44 +0300

    add getEmail method

--- emailclient/src/app/inbox/email.service.ts
+++ emailclient/src/app/inbox/email.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface EmailSummary {
+  id: string,
+  subject: string,
+  from: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
+  rootUrl = 'https://api.angular-email.com'
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getEmails() {
+    return this.httpClient.get<EmailSummary[]>(`${this.rootUrl}/emails`);
+  }
 }
