4e10e3c3ff52a31431b4be727bd426d452e3b966
Dmitry <3ddesign@bk.ru>
2020-09-11 09:17:46 +0300

    add not-found component

--- emailclient/src/app/inbox/email-resolver.service.ts
+++ emailclient/src/app/inbox/email-resolver.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { EMPTY } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Email } from './email';
 import { EmailService } from './email.service';
 
@@ -7,11 +9,17 @@ import { EmailService } from './email.service';
   providedIn: 'root'
 })
 export class EmailResolverService implements Resolve<Email> {
-  constructor(private emailService: EmailService) {}
+  constructor(private emailService: EmailService, private router: Router) {}
 
   resolve(route: ActivatedRouteSnapshot) {
     const { id } = route.params;
 
-    return this.emailService.getEmail(id);
+    return this.emailService.getEmail(id).pipe(
+      catchError(() => {
+        this.router.navigateByUrl('/inbox/not-found');
+
+        return EMPTY;
+      })
+    );
   }
 }
--- emailclient/src/app/inbox/inbox-routing.module.ts
+++ emailclient/src/app/inbox/inbox-routing.module.ts
@@ -5,12 +5,17 @@ import { HomeComponent } from './home/home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { EmailShowComponent } from './email-show/email-show.component';
 import { EmailResolverService } from './email-resolver.service';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     children: [
+      {
+        path: 'not-found',
+        component: NotFoundComponent
+      },
       {
         path: ':id',
         component: EmailShowComponent,
