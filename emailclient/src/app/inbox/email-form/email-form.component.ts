af8fc92303391db39cb132ba6a0c82a56e11f6b2
Dmitry <3ddesign@bk.ru>
2020-09-22 08:44:38 +0300

    add form validation

--- emailclient/src/app/inbox/email-form/email-form.component.ts
+++ emailclient/src/app/inbox/email-form/email-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Email } from '../email';
 
 @Component({
@@ -17,10 +17,10 @@ export class EmailFormComponent implements OnInit {
     const { subject, from, to, text } = this.email;
 
     this.emailForm = new FormGroup({
-      to: new FormControl(to),
-      from: new FormControl(from),
-      subject: new FormControl(subject),
-      text: new FormControl(text)
+      to: new FormControl(to, [Validators.required, Validators.email]),
+      from: new FormControl({ value: from, disabled: true }),
+      subject: new FormControl(subject, [Validators.required]),
+      text: new FormControl(text, [Validators.required])
     });
   }
 }
