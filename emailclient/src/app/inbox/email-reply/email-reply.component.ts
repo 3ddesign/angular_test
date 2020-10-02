ec3c47125a2899ea762d8c9e31f1b651eeb25f9b
Dmitry <3ddesign@bk.ru>
2020-10-02 08:57:52 +0300

    add formatting reply emails

--- emailclient/src/app/inbox/email-reply/email-reply.component.ts
+++ emailclient/src/app/inbox/email-reply/email-reply.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Email } from '../email';
 
 @Component({
@@ -8,10 +8,21 @@ import { Email } from '../email';
 })
 export class EmailReplyComponent implements OnInit {
   showModal = false;
+  @Input() email: Email;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const text = this.email.text.replace(/\n/gi, '\n> ');
+
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: `RE: ${this.email.subject}`,
+      text: `\n\n\n-------- ${this.email.from} wrote:\n> ${text}`
+    };
+  }
 
   onSubmit(email: Email) {}
 }
