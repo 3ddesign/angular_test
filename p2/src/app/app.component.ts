2c951b91f1ee711f5fc5ccbb6fb2d69e788c1d3a
Dmitry <3ddesign@bk.ru>
2020-10-30 06:27:37 +0200

    remove p2

--- p2/src/app/app.component.ts
+++ p2/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'landing';
+}
--- p2/src/app/app.module.ts
+++ p2/src/app/app.module.ts
@@ -1,39 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-export const APP_ID = 'my-app';
-
-// import ngx-translate and the http loader
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MaterialModule } from './modules/material/material.module';
-
 @NgModule({
-  imports: [
-    BrowserAnimationsModule,
-    BrowserModule.withServerTransition({ appId: APP_ID }),
-    AppRoutingModule,
-    MaterialModule,
-    TranslateModule.forRoot({
-      loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-      }
-  })
+  declarations: [
+    AppComponent
   ],
-  exports: [ AppRoutingModule ],
+  imports: [
+    BrowserModule
+  ],
   providers: [],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-// required for AOT compilation
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
