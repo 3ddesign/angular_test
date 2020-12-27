8cb9926fe5d2e93d55c3a140cee33f6f39f08382
Dmitry <3ddesign@bk.ru>
2020-12-27 10:00:03 +0200

    remove shared imports

--- multiApp/src/app/app.module.ts
+++ multiApp/src/app/app.module.ts
@@ -1,35 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { App1SharedModule }
-       from "../../projects/app1/src/app/app.module";
-import { App2SharedModule }
-       from "../../projects/app2/src/app/app.module";
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    App1SharedModule.forRoot(),
-    App2SharedModule.forRoot()
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
-@NgModule({})
-export class App1SharedModule{
-  static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: AppModule,
-      providers: []
-    }
-  }
-}
+export class AppModule {}
