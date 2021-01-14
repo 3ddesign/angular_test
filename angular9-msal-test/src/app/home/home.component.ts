d6bc2df94d5a79885ee43db2558cc405d0dc3fa2
Dmitry <3ddesign@bk.ru>
2021-01-14 10:23:27 +0200

    test msal-angular

---
+++ angular9-msal-test/src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+import { AuthenticationResult } from '@azure/msal-browser';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private authService: MsalService) { }
+
+  ngOnInit(): void {
+    this.authService.handleRedirectObservable().subscribe({
+      next: (result: AuthenticationResult) => {
+        if (result) {
+          this.authService.instance.setActiveAccount(result.account);
+          console.log(result);
+        }
+      },
+      error: (error) => console.log(error)
+    });
+  }
+
+}
