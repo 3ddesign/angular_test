d191e1aaff74b0e635e5c3390e8fcddbdb20d44c
Dmitry <3ddesign@bk.ru>
2021-09-30 09:57:06 +0300

    add spreadjs v14 example

---
+++ spreadjs/v14/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    appHeader = 'SpreadJS Angular Sample';
+    appFooter = '© 2021 GrapeCity, inc. All Rights Reserved. All product and company names herein may be trademarks of their respective owners.';
+}
