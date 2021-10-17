25669015e62596cd2e474582b435c5ea37ee6589
Dmitry <3ddesign@bk.ru>
2021-10-17 09:18:27 +0300

    add example of dynamic with APP_INITIALIZER

--- dynamic_config/dc/src/app/app.component.ts
+++ dynamic_config/dc/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { environment } from './../environments/environment';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dc';
+  title = 'dynamic-config';
+
+  public get environmentName() {
+    return environment.environment;
+  }
 }
--- dynamic_config/dc/src/app/app.module.ts
+++ dynamic_config/dc/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppInitService } from './app-init.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,19 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AppInitService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: init_app,
+      deps: [AppInitService],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function init_app(appLoadService: AppInitService) {
+  return () => appLoadService.init();
+}
